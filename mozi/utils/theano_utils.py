2b8e5575c62cd4951fc080664e3a1d096b9d1564
Hycis <Hycis@Hyciss-MacBook-Pro.local>
2015-10-27 00:36:51 +0800

    changed to floatX

--- mozi/utils/theano_utils.py
+++ mozi/utils/theano_utils.py
@@ -3,24 +3,25 @@ import numpy as np
 import theano
 import theano.tensor as T
 
+floatX = theano.config.floatX
 '''
 from keras
 '''
 
 def asfloatX(X):
-    return np.asarray(X, dtype=theano.config.floatX)
+    return np.asarray(X, dtype=floatX)
 
-def sharedX(X, dtype=theano.config.floatX, name=None):
+def sharedX(X, dtype=floatX, name=None):
     return theano.shared(np.asarray(X, dtype=dtype), name=name)
 
-def shared_zeros(shape, dtype=theano.config.floatX, name=None):
+def shared_zeros(shape, dtype=floatX, name=None):
     return sharedX(np.zeros(shape), dtype=dtype, name=name)
 
-def shared_scalar(val=0., dtype=theano.config.floatX, name=None):
+def shared_scalar(val=0., dtype=floatX, name=None):
     return theano.shared(np.cast[dtype](val))
 
-def shared_ones(shape, dtype=theano.config.floatX, name=None):
+def shared_ones(shape, dtype=floatX, name=None):
     return sharedX(np.ones(shape), dtype=dtype, name=name)
 
 def alloc_zeros_matrix(*dims):
-    return T.alloc(np.cast[theano.config.floatX](0.), *dims)
+    return T.alloc(np.cast[floatX](0.), *dims)
