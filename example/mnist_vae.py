e277ee66e052077934c1c02a4ecf3258768775b8
hycis <hyciswu@gmail.com>
2015-11-19 13:13:13 +0800

    move setenv to env.py

--- example/mnist_vae.py
+++ example/mnist_vae.py
@@ -13,27 +13,7 @@ from mozi.train_object import TrainObject
 from mozi.cost import SGVB_bin
 from mozi.learning_method import *
 from mozi.weight_init import *
-
-
-def setenv():
-    NNdir = os.path.dirname(os.path.realpath(__file__))
-    NNdir = os.path.dirname(NNdir)
-
-    # directory to save all the dataset
-    if not os.getenv('MOZI_DATA_PATH'):
-        os.environ['MOZI_DATA_PATH'] = NNdir + '/data'
-
-    # directory for saving the database that is used for logging the results
-    if not os.getenv('MOZI_DATABASE_PATH'):
-        os.environ['MOZI_DATABASE_PATH'] = NNdir + '/database'
-
-    # directory to save all the trained models and outputs
-    if not os.getenv('MOZI_SAVE_PATH'):
-        os.environ['MOZI_SAVE_PATH'] = NNdir + '/save'
-
-    print('MOZI_DATA_PATH = ' + os.environ['MOZI_DATA_PATH'])
-    print('MOZI_SAVE_PATH = ' + os.environ['MOZI_SAVE_PATH'])
-    print('MOZI_DATABASE_PATH = ' + os.environ['MOZI_DATABASE_PATH'])
+from mozi.env import setenv
 
 
 def train():
--- example/voc_alexnet.py
+++ example/voc_alexnet.py
@@ -10,29 +10,10 @@ from mozi.log import Log
 from mozi.train_object import TrainObject
 from mozi.cost import error, entropy
 from mozi.learning_method import SGD
+from mozi.env import setenv
+
 import os
-
 import theano
-
-def setenv():
-    NNdir = os.path.dirname(os.path.realpath(__file__))
-    NNdir = os.path.dirname(NNdir)
-
-    # directory to save all the dataset
-    if not os.getenv('MOZI_DATA_PATH'):
-        os.environ['MOZI_DATA_PATH'] = NNdir + '/data'
-
-    # directory for saving the database that is used for logging the results
-    if not os.getenv('MOZI_DATABASE_PATH'):
-        os.environ['MOZI_DATABASE_PATH'] = NNdir + '/database'
-
-    # directory to save all the trained models and outputs
-    if not os.getenv('MOZI_SAVE_PATH'):
-        os.environ['MOZI_SAVE_PATH'] = NNdir + '/save'
-
-    print('MOZI_DATA_PATH = ' + os.environ['MOZI_DATA_PATH'])
-    print('MOZI_SAVE_PATH = ' + os.environ['MOZI_SAVE_PATH'])
-    print('MOZI_DATABASE_PATH = ' + os.environ['MOZI_DATABASE_PATH'])
 
 
 def train():
