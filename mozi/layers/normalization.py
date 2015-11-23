fixed divide by zero in normalization

mozi/layers/normalization.py
@@ -21,7 +21,7 @@ class BatchNormalization(Template):
             epsilon:
                 denominator min value for preventing division by zero in computing std
         '''
-        self.epsilon = 1.0
+        self.epsilon = 1e-6
         self.input_shape = input_shape
         self.mem = short_memory
 
@@ -36,7 +36,10 @@ class BatchNormalization(Template):
 
     def _train_fprop(self, state_below):
         miu = state_below.mean(axis=0)
-        std = T.std(state_below, axis=0)
+        # std = T.std(state_below, axis=0)
+        var = (state_below ** 2).mean(axis=0) - miu ** 2
+        std = T.sqrt(var)
+        # std = T.mean((X - m) ** 2 + self.epsilon, axis=0) ** 0.5
         self.moving_mean += self.mem * miu + (1-self.mem) * self.moving_mean
         self.moving_std += self.mem * std + (1-self.mem) * self.moving_std
         denom = T.clip(self.moving_std, self.epsilon, self.moving_std)