fixed divide by zero in normalization

mozi/layers/normalization.py
@@ -39,7 +39,7 @@ class BatchNormalization(Template):
         # std = T.std(state_below, axis=0)
         # var = (state_below ** 2).mean(axis=0) - miu ** 2
         # std = T.sqrt(var)
-        std = T.mean((X - m) ** 2 + self.epsilon, axis=0) ** 0.5
+        std = T.mean((state_below - miu) ** 2 + self.epsilon, axis=0) ** 0.5
         self.moving_mean += self.mem * miu + (1-self.mem) * self.moving_mean
         self.moving_std += self.mem * std + (1-self.mem) * self.moving_std
         # denom = T.clip(self.moving_std, self.epsilon, self.moving_std)