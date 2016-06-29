d523d7a08a5af77533ecf6845aa5a3fc83e883d2
Wu <zhenzhou.wu@sap.com>
2016-06-29 13:35:54 +0800

    added theano from requirement

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='hyciswu@gmail.com',
     install_requires=['numpy>=1.7.1', 'scipy>=0.11',
                       'six>=1.9.0', 'scikit-learn>=0.17', 'pandas>=0.17',
-                      'matplotlib>=1.5'],
+                      'matplotlib>=1.5', 'Theano>=0.8'],
     url='https://github.com/hycis/Mozi',
     license='The MIT License (MIT), see LICENCE',
     description='Deep learning package based on theano for building all kinds of models',
