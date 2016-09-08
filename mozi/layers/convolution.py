f29917c46c7d7d5085589cf5e3783519a88b5243
Wu <zhenzhou.wu@sap.com>
2016-09-08 11:56:28 +0800

    conv2d from nnet to signal

--- mozi/layers/convolution.py
+++ mozi/layers/convolution.py
@@ -10,6 +10,8 @@ from theano.tensor.signal import downsample
 from mozi.weight_init import XavierUniformWeight, GaussianWeight
 from mozi.layers.template import Template
 from mozi.utils.theano_utils import shared_zeros
+# from theano.tensor.nnet.conv import conv2d
+from theano.tensor.signal.conv import conv2d
 
 floatX = theano.config.floatX
 
@@ -43,7 +45,7 @@ class Convolution2D(Template):
 
 
     def _train_fprop(self, state_below):
-        conv_out = theano.tensor.nnet.conv.conv2d(state_below, self.W,
+        conv_out = conv2d(state_below, self.W,
                                                   border_mode=self.border_mode,
                                                   subsample=self.stride)
         return conv_out + self.b.dimshuffle('x', 0, 'x', 'x')
